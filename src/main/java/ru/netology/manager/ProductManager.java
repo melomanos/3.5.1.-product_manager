8c887d21af7f3762a81fcb7fbaa4b052f19363f8
melomanos <philippov_mihail@mail.ru>
2020-11-04 13:28:33 +0300

    Second task reworked

--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -1,11 +1,13 @@
 package ru.netology.manager;
 
-import ru.netology.domain.Book;
 import ru.netology.domain.Product;
-import ru.netology.domain.Smartphone;
 
 public class ProductManager {
     private Product[] items = new Product[0];
+
+//    public void setItems(Product[] items) {
+//        this.items = items;
+//    }
 
     public void add(Product item) {
         int length = items.length + 1;
@@ -19,7 +21,7 @@ public class ProductManager {
     public Product[] searchBy(String text) {
         Product[] result = new Product[0];
         for (Product product: findAll()) {
-            if (matches(product, text)) {
+            if (product.matches(text)) {
                 Product[] tmp = new Product[result.length + 1];
                 System.arraycopy(result, 0, tmp, 0, result.length);
                 tmp[tmp.length - 1] = product;
@@ -29,28 +31,28 @@ public class ProductManager {
         return result;
     }
 
-    public boolean matches(Product product, String search) {
-        if (product instanceof Book) {
-            Book book = (Book) product;
-            if (book.getName().equalsIgnoreCase(search)) {
-                return true;
-            }
-            if (book.getAuthor().equalsIgnoreCase(search)) {
-                return true;
-            }
-        }
-
-        if (product instanceof Smartphone) {
-            Smartphone smartphone = (Smartphone) product;
-            if (smartphone.getName().equalsIgnoreCase(search)) {
-                return true;
-            }
-            if (smartphone.getManufacturer().equalsIgnoreCase(search)) {
-                return true;
-            }
-        }
-        return false;
-    }
+//    public boolean matches(Product product, String search) {
+//        if (product instanceof Book) {
+//            Book book = (Book) product;
+//            if (book.getName().equalsIgnoreCase(search)) {
+//                return true;
+//            }
+//            if (book.getAuthor().equalsIgnoreCase(search)) {
+//                return true;
+//            }
+//        }
+//
+//        if (product instanceof Smartphone) {
+//            Smartphone smartphone = (Smartphone) product;
+//            if (smartphone.getName().equalsIgnoreCase(search)) {
+//                return true;
+//            }
+//            if (smartphone.getManufacturer().equalsIgnoreCase(search)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
     public Product[] findAll() {
         return items;
